617b337c23b602f09e3904c836a68f0a0561204c
ksai-krishna <ksaikrishna5601@gmail.com>
2025-01-01 22:05:34 +0530

    Initial commit

---
+++ code/voice_to_text.py
@@ -0,0 +1,16 @@
+### setup #### 
+# pip install SpeechRecognition
+###############
+
+import speech_recognition as sr
+def voice_to_text():
+    recognizer = sr.Recognizer()
+    
+    with sr.Microphone() as source:
+        print("Listening ...")
+        audio = recognizer.listen(source)
+        print("Processing your speech...")
+        text = recognizer.recognize_google(audio)
+        print(f"You said: {text}")
+
+voice_to_text()
